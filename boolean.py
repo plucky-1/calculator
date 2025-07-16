287d4beebccd4441e3fa6dacf8a7702902f250b3
plucky-1 <bwehfaaaron@gmail.com>
2025-07-16 14:23:18 +0100

    foordivision added

--- boolean.py
+++ boolean.py
@@ -5,6 +5,7 @@ print("""*******
 2.Subtraction
 3.Multiplication
 4.Division
+  Exponential
 ************""") 
 
 print("Enter two numbers  Add")
@@ -14,6 +15,7 @@ firstNumber = input("first number:")
 secondNumber = input("secondNumber:")
 sum = float(firstNumber) +  float(secondNumber)
 print(f"{firstNumber} + {secondNumber} = {sum :.2f}")
+
 print("***********")
 print("Subtraction")
 print("Enter two numbers to subtract")
@@ -23,6 +25,7 @@ firstNumber = input("first number:")
 secondNumber = input("secondNumber:")
 sub = float(firstNumber) -  float(secondNumber)
 print(f"{firstNumber} - {secondNumber} = {sub :.2f}")
+
 print("Divission")
 print("Enter two numbers to Divid")
 #promt the user for a number and store it
@@ -31,6 +34,7 @@ firstNumber = input("first number:")
 secondNumber = input("secondNumber:")
 div = float(firstNumber) /  float(secondNumber)
 print(f"{firstNumber} / {secondNumber} = {div :.2f}")
+
 print("Exponetial")
 print("Enter two numbers for Exponential")
 #promt the user for a number and store it
@@ -40,5 +44,12 @@ secondNumber = input("secondNumber:")
 expo = float(firstNumber) **  float(secondNumber)
 print(f"{firstNumber} ** {secondNumber} = {expo :.2f}")
 
-
+print("Floordivision")
+print("Enter two numbers to perform floordivition")
+#promt the user for a number and store it
+firstNumber = input("first number:")
+#prompt user for a second number and store it
+secondNumber = input("secondNumber:")
+floordiv = float(firstNumber) //  float(secondNumber)
+print(f"{firstNumber} // {secondNumber} = {floordiv :.2f}")
 
