0c50e4ffdedaaeb40393cbf1e79781a4092eaf64
plucky-1 <bwehfaaaron@gmail.com>
2025-07-16 13:17:00 +0100

    initial

---
+++ boolean.py
@@ -0,0 +1,44 @@
+# Asimple calculator App
+
+print("""*******
+1.Addition
+2.Subtraction
+3.Multiplication
+4.Division
+************""") 
+
+print("Enter two numbers  Add")
+#promt the user for a number and store it
+firstNumber = input("first number:")
+#prompt user for a second number and store it
+secondNumber = input("secondNumber:")
+sum = float(firstNumber) +  float(secondNumber)
+print(f"{firstNumber} + {secondNumber} = {sum :.2f}")
+print("***********")
+print("Subtraction")
+print("Enter two numbers to subtract")
+#promt the user for a number and store it
+firstNumber = input("first number:")
+#prompt user for a second number and store it
+secondNumber = input("secondNumber:")
+sub = float(firstNumber) -  float(secondNumber)
+print(f"{firstNumber} - {secondNumber} = {sub :.2f}")
+print("Divission")
+print("Enter two numbers to Divid")
+#promt the user for a number and store it
+firstNumber = input("first number:")
+#prompt user for a second number and store it
+secondNumber = input("secondNumber:")
+div = float(firstNumber) /  float(secondNumber)
+print(f"{firstNumber} / {secondNumber} = {div :.2f}")
+print("Exponetial")
+print("Enter two numbers for Exponential")
+#promt the user for a number and store it
+firstNumber = input("first number:")
+#prompt user for a second number and store it
+secondNumber = input("secondNumber:")
+expo = float(firstNumber) **  float(secondNumber)
+print(f"{firstNumber} ** {secondNumber} = {expo :.2f}")
+
+
+
